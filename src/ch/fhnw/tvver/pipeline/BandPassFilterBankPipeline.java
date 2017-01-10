6411f3e1a315adcadfd78a7dce12c457c16a9cdf
Michel <michel.pluess@students.fhnw.ch>
2017-01-10 16:06:02 +0100

    .

--- src/ch/fhnw/tvver/pipeline/BandPassFilterBankPipeline.java
+++ src/ch/fhnw/tvver/pipeline/BandPassFilterBankPipeline.java
@@ -13,12 +13,13 @@ import ch.fhnw.ether.media.RenderCommandException;
 import ch.fhnw.ether.media.RenderProgram;
 import ch.fhnw.tvver.AbstractPCM2MIDI;
 import ch.fhnw.tvver.rendercommand.BandPassFilterBank;
+import ch.fhnw.tvver.rendercommand.BandPassOnsetDetectADSR;
 import ch.fhnw.tvver.rendercommand.PerfectMIDIDetection;
 import ch.fhnw.tvver.rendercommand.PrintMeanOfPerfectDetection;
 
 public class BandPassFilterBankPipeline extends AbstractPCM2MIDI {
 	public BandPassFilterBankPipeline(File track) throws UnsupportedAudioFileException, IOException, MidiUnavailableException, InvalidMidiDataException, RenderCommandException {
-		super(track, EnumSet.of(Flags.MAX_SPEED));
+		super(track, EnumSet.of(Flags.REPORT, Flags.MAX_SPEED));
 	}
 	
 	@Override
@@ -30,7 +31,7 @@ public class BandPassFilterBankPipeline extends AbstractPCM2MIDI {
 		BandPassFilterBank bandPassFilterBank = new BandPassFilterBank(24, 101);
 		program.addLast(bandPassFilterBank);
 		
-		//program.addLast(new BandPassOnsetDetectADSR(bandPassFilterBank, this));
+		program.addLast(new BandPassOnsetDetectADSR(bandPassFilterBank, this));
 		//program.addLast(new BandPassOnsetDetectBigChange(bandPassFilterBank, this));
 		//program.addLast(new BandPassOnsetDetectMarkovTrain(ppd, bandPassFilterBank));
 		
